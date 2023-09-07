dc8f38c0acd7ede803e7ccd8ea18f328a8356004
mpvermette <marie-pier.vermette@hotmail.com>
2023-09-06 21:16:09 -0400

    finition des pages et début du formulaire

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v27';
+const CACHE_NAME = 'static-cache-v28';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
