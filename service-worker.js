8bc113b02cc6823b002967df64f4021fb863b76e
mpvermette <marie-pier.vermette@hotmail.com>
2023-09-11 14:12:03 -0400

    changement des icones

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v29';
+const CACHE_NAME = 'static-cache-v30';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
